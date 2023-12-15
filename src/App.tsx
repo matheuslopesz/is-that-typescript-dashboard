3d12b444c165b9878523b662b809707b54fc320d
matheus lopes <matheuslopes.developer@gmail.com>
2023-12-15 00:31:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Sidenav from './components/SideNav'
+import OverView from './page/OverView'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Sidenav />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideNav: React.FC = () => {
+  return (
+    <div className="side-nav">
+      {/* Add your side navigation content here */}
+    </div>
+  );
+};
+
+export default SideNav;
