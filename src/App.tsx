0046a1b36d9b53497a4943ddde7023db6bba337c
matheus lopes <matheuslopes.developer@gmail.com>
2023-12-15 22:02:09 -0300

    Add DataContextProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import './App.css'
 import Header from './components/Header'
 import Sidenav from './components/SideNav'
 import OverView from './pages/OverView'
+import { DataContextProvider } from './contexts/DataContext'
 
 function App() {
   return (
     <>
-      <Sidenav />
-      <main>
-        <Header />
-        <OverView />
-      </main>
+      <DataContextProvider>
+        <Sidenav />
+        <main>
+          <Header />
+          <OverView />
+        </main>
+      </DataContextProvider>
     </>
   )
 }
