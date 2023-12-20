cb873936fcd6817a40ff2458664390e9a1938f24
matheus lopes <matheuslopes.developer@gmail.com>
2023-12-20 20:36:35 -0300

    Add react-router-dom and create Sale component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,26 @@ import Sidenav from './components/SideNav'
 import OverView from './pages/OverView'
 import { DataContextProvider } from './contexts/DataContext'
 import Sales from './pages/Sales'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Sale } from './pages/Sale'
 
 function App() {
   return (
     <>
-      <DataContextProvider>
-        <div className='container'>
-          <Sidenav />
-          <main>
-            <Header />
-            <OverView />
-            <Sales />
-          </main>
-        </div>
-      </DataContextProvider>
+      <BrowserRouter>
+        <DataContextProvider>
+          <div className='container'>
+            <Sidenav />
+            <main>
+              <Header />
+              <Routes>
+                <Route path='/'  element={ <OverView/> } />
+                <Route path='/'  element={ <Sales/> } />
+              </Routes>
+            </main>
+          </div>
+        </DataContextProvider>
+      </BrowserRouter>
     </>
   )
 }
