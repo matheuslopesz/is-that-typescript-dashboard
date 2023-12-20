9573d90ad7395183aafb0b464afc2f2ac1acb920
matheus lopes <matheuslopes.developer@gmail.com>
2023-12-20 20:50:02 -0300

    Update SaleItem component to use NavLink instead of anchor tag

--- src/components/SaleItem.tsx
+++ src/components/SaleItem.tsx
@@ -1,12 +1,13 @@
 import { ISale } from "../contexts/DataContext";
+import { NavLink } from "react-router-dom";
 
 
 const SaleItem = ({ sale }: { sale: ISale }) => {
   return (
     <div className="venda box">
-      <a href="" style={{ fontFamily: "monospace" }}>
+       <NavLink to={`/vendas/${sale.id}`} style={{ fontFamily: "monospace" }}>
         {sale.id}
-      </a>
+      </NavLink>
       <div>{sale.nome}</div>
       <div>
         {sale.preco.toLocaleString("pt-br", {
